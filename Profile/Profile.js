00189a88f68c45136ef6687706f08ab5eaea442c
Lê Hoài Nam19 <hoainam1945@gmail.com>
2025-06-28 16:36:31 +0700

    update

--- Profile/Profile.js
+++ Profile/Profile.js
@@ -1,1 +1,23 @@
-console.log("Trang Profile đã sẵn sàng!");
+
+const icons = document.querySelectorAll(".icon");
+const sections = document.querySelectorAll(".main-tab");
+
+icons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    const target = icon.getAttribute("data-target") + "-section";
+
+    // Ẩn toàn bộ section
+    sections.forEach(section => section.classList.remove("active"));
+    icons.forEach(i => i.classList.remove("active"));
+
+    // Hiện đúng section
+    document.getElementById(target).classList.add("active");
+    icon.classList.add("active");
+  });
+});
+
+
+
+
+
+
