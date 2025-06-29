f32e81ba4a54b8d83ee3ea19da0bcd6e72983b26
QuangHuy8905 <huydq7436@ut.edu.vn>
2025-06-29 14:26:48 +0700

    update

--- Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-const icons = document.querySelectorAll(".icon");
-const sections = document.querySelectorAll(".main-tab");
-
-// Đảm bảo phần profile luôn active và icon profile luôn được làm nổi bật khi không có các tab khác
-sections.forEach(section => section.classList.remove("active"));
-icons.forEach(i => i.classList.remove("active"));
-
-document.getElementById("profile-section").classList.add("active");
-document.querySelector('.sidebar .icon[data-target="profile"]').classList.add("active");
