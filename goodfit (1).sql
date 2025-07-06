585d4ac1312d7992553fc7d490dc052b11b16705
QuangHuy8905 <huydq7436@ut.edu.vn>
2025-07-06 11:15:02 +0700

    up

--- goodfit (1).sql
+++ goodfit (1).sql
@@ -1,44 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 05, 2025 at 08:39 AM
--- Server version: 9.2.0
--- PHP Version: 8.2.12
+-- Database: `goodfit`
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `goodfit`
---
-
+-- --------------------------------------------------------
+-- Table structure for table `sanpham`
 -- --------------------------------------------------------
 
---
--- Table structure for table `sanpham`
---
-
 CREATE TABLE `sanpham` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `ten` varchar(255) NOT NULL,
   `gia` int NOT NULL,
   `anh` varchar(255) NOT NULL,
   `mota` text,
-  `in_cart` tinyint DEFAULT '0'
+  `in_cart` tinyint DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `sanpham`
---
+-- Data for table `sanpham`
 
 INSERT INTO `sanpham` (`id`, `ten`, `gia`, `anh`, `mota`, `in_cart`) VALUES
 (3, 'Bộ Tạ Tay', 999000, 'img/sanpham2.jpg', 'Bộ tạ tay đa năng phù hợp cho người mới bắt đầu và tập luyện tại nhà.', 0),
@@ -55,20 +36,18 @@ INSERT INTO `sanpham` (`id`, `ten`, `gia`, `anh`, `mota`, `in_cart`) VALUES
 (14, 'Máy Tập Bụng Mini', 500000, 'img/maytapbung.jpg', 'Máy hỗ trợ gập bụng và tập cơ core.', 0);
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `users`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
+-- Data for table `users`
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (8, 'ledat16042005@gmail.com', '$2y$10$vCyq4DxslsoijqO20cDtr.RRWKwpbHoBxeYSxMB0YrM4Ho9Oon3ji'),
@@ -78,40 +57,43 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (12, 'QUANG DAT', '$2y$10$KSjBu9jMFqWfh4ZH6ooRd.shHZX6pNR9EMQ1Dv894Z/.ULjhgB2V6'),
 (13, 'HUY', '$2y$10$09D1DIM.l3t6hd.M1ZHhVumMFYbif..L48Pi.XgbhbXd6noiMUNlG');
 
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
+-- Table structure for table `videos`
+-- --------------------------------------------------------
 
---
--- Indexes for table `sanpham`
---
-ALTER TABLE `sanpham`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE `videos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tacgia` VARCHAR(255),
+  `tieude` VARCHAR(255) NOT NULL,
+  `thumbnail` VARCHAR(255),
+  `video_url` VARCHAR(255) NOT NULL,
+  `luotxem` INT DEFAULT 0,
+  `thoigian` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+-- Data for table `videos`
 
---
--- AUTO_INCREMENT for dumped tables
---
+INSERT INTO `videos` (`tacgia`, `tieude`, `thumbnail`, `video_url`, `luotxem`, `thoigian`) VALUES
+("SHINPHAMM", "10 phút TẬP BỤNG đơn giản TẠI NHÀ | AbsWorkout Routine | SHINPHAMM", "img/video1.png", "https://www.youtube.com/watch?v=REwT4IjE_eY&t=698s", 11000000, '2020-07-01'),
+("NGUYEN HIEU YOGA", "Yoga cơ bản tại nhà - Bài 1: kéo dãn", "img/anh3.jpg", "https://www.youtube.com/watch?v=JUZ_6Yh9eUw", 29000000, '2016-07-01'),
+("VIET THUY EROBIS", "20 PHÚT AEROBIC GIẢM MỠ TOÀN THÂN... | BÀI 6", "img/anh5.jpg", "https://www.youtube.com/watch?v=eHnwaqhR11U", 19000000, '2019-07-01'),
+("TUNG PHAO DUNG", "15 Phút Cardio ĐỐT MỠ THỪA Toàn Thân...", "img/anh7.jpg", "https://www.youtube.com/watch?v=GyvdcXgzxCE", 3100000, '2020-07-01'),
+("GYM CHANGE 72", "10 Minutes of Visual Power & Beats – 6-Pack Energy", "img/anh8.png", "https://www.youtube.com/watch?v=muDIuysxOxU", 77, CURRENT_DATE - INTERVAL 13 DAY),
+("SCULPTZONE", "20 MIN HIIT TABATA...Burn 400 Calories", "img/anh9.jpg", "https://www.youtube.com/watch?v=qiPWEgO-bJc", 537000000, CURRENT_DATE - INTERVAL 4 MONTH),
+("EMI WONG", "FIX & SLIM YOUR BACK...in 10 minutes ~ Emi", "img/anh10.jpg", "https://www.youtube.com/watch?v=riS89biXH7E", 28000000, '2022-07-01'),
+("GYMDAYROI", "8 Phút Tập Luyện GIẢM MỠ BẮP TAY...", "img/anh11.jpg", "https://www.youtube.com/watch?v=RkvZpXcF-jo", 7900000, '2020-07-01'),
+("QUYEN NGUYEN", "XOAY HÈ KHÁC LẠ, HƯỚNG DẪN CHẬM...", "img/anh12.jpg", "https://www.youtube.com/watch?v=QjC-LvggQAw", 46000000, '2020-07-01'),
+("KOCHAN-THU HON", "30p tổng hợp Tái Sinh,bài số 52", "img/anh13.jpg", "https://www.youtube.com/watch?v=v9PDbF-Av2g", 114000000, CURRENT_DATE - INTERVAL 2 MONTH),
+("MILESTON", "20 PHÚT TẬP LUYỆN TOÀN THÂN...", "img/anh14.jpg", "https://www.youtube.com/watch?v=wIynl3at0Rs", 3500000, '2020-07-01'),
+("x DATIFFN", "20 Minute Full Body Workout (No Equipment)", "img/anh15.jpg", "https://www.youtube.com/watch?v=Yu20j5jGHTc", 3100000, '2022-07-01');
 
---
--- AUTO_INCREMENT for table `sanpham`
---
-ALTER TABLE `sanpham`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+-- --------------------------------------------------------
+
+-- Set auto increment values
+ALTER TABLE `sanpham` MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+ALTER TABLE `users` MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+ALTER TABLE `videos` MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
